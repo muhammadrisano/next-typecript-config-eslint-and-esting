6f5345c5d40827431fd1c663300ec120cf831ece
risano <muhammadrisano@gmail.com>
2021-09-22 23:06:50 +0700

    inital app

---
+++ src/components/Hello.spec.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import { Hello } from "./Hello";
+test("render 'hello world'", () => {
+  render(<Hello />);
+  const myElement = screen.getByText(/Hello World/);
+  expect(myElement).toBeInTheDocument();
+});
---
+++ src/components/sum.spec.ts
@@ -0,0 +1,6 @@
+import { sum } from "./sum";
+
+test("suming 5 and 2 will return 7", () => {
+  expect(5).toBe(5);
+  expect(sum(2, 5)).toBe(7);
+});
